c5406abcf673cf4252fac42642129474e5b5d48f
Harshal <harshaljadhav792@gmail.com>
2026-09-16 21:53:06 +0530

    sub arr sum as k

---
+++ 1.SubArray/subarrsumk.java
@@ -0,0 +1,19 @@
+class SubArrSumK{
+	public  static void main(String [] args){
+		int arr [] = {1,2,3};		
+		int target = 3;
+		int count = 0;
+		for(int i=0; i<arr.length; i++){
+			for(int j=i; j<arr.length; j++){
+				int sum = 0;
+				for(int k=i; k<=j; k++){
+					sum += arr[k];
+				}
+				if(target == sum){
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
